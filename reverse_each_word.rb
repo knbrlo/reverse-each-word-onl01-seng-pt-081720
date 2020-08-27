074c6691a5b1f9d646698cda04a92ed953296c96
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 23:40:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   final_array = []
   array_words = sentence.split(" ")
   array_words.each do |word, index|
-    final_array[index] = word.reverse
+    final_array << word.reverse
   end
   final_array.join(" ")
 end
