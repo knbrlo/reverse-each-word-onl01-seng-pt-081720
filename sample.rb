df8fc607bdf2da29c12392c42206c16e3eb2afd1
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 23:45:10 +0000

    Automatically backed up by Learn

--- sample.rb
+++
@@ -1,9 +0,0 @@
-toppings = ["pickles", "mushrooms", "bacon"]
-
-def hamburger(toppings)
-   toppings.collect do |topping|
-   puts "I love #{topping} on my burgers!"
-   end
-end
-
-hamburger(toppings)
